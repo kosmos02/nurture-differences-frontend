e2b5945f7e377c7a92e396d6924af1a9d57ea0c1
kosmos02 <alexandrgabe@gmail.com>
2021-04-20 11:04:52 -0600

    tooltip now only displays country name. USA is green now.

--- src/App.js
+++ src/App.js
@@ -34,11 +34,11 @@ export default class App extends Component {
     
   stylingFunction = (context) => {
     const opacityLevel = 0.1 + (1.5 * (context.countryValue - context.minValue) / (context.maxValue - context.minValue))
-    return { fill: context.country === "US" ? "blue" : context.color, fillOpacity: opacityLevel, stroke: "green", strokeWidth: 1, strokeOpacity: 0.2, cursor: "pointer" }
+    return {fill: context.color, fillOpacity: opacityLevel, stroke: "green", strokeWidth: 1, strokeOpacity: 0.2, cursor: "pointer" }
   }
 
   tooltipText = (text) => {
-    console.log(text)
+    return text
   }
 
   render(){
