27149e3d92c20c9baad6e9697c1e123df1ff796f
dbhmunro <dbhmunro@gmail.com>
2021-04-21 18:29:35 -0400

    date column header to show current month and navigation arrows

--- src/components/countryDetails.js
+++ src/components/countryDetails.js
@@ -1,4 +1,7 @@
 import {useState, useEffect} from 'react'
+import rightArrow from '../assets/nav-right.svg'
+import leftArrow from '../assets/nav-left.svg'
+
 
 export function CountryDetails(props) {
 
@@ -7,13 +10,14 @@ const [holidays, setHolidays] = useState([])
 let getCountryHolidays = (countryCode) => {
     // let year = '2020';
     let year = new Date().getFullYear().toString();
-
+    let month = (new Date().getMonth() + 1).toString();
+    
     //Holiday API key
     // const apiKey = '580d9214-e005-4391-9de5-8b111c8bbd0e';
     // fetch(`https://holidayapi.com/v1/holidays/?key=${apiKey}&country=${countryCode}&year=${year}`)
     //Calendarific API key
     const apiKey = 'a207d49e5afb124557eee979ab1b7ca2cc315135';
-    fetch(`https://calendarific.com/api/v2/holidays?&api_key=${apiKey}&country=${countryCode}&year=${year}`)
+    fetch(`https://calendarific.com/api/v2/holidays?&api_key=${apiKey}&country=${countryCode}&year=${year}&month=${month}`)
     // when using the API above, replace observed with holiday type
         .then(response => response.json())
         .then(results => {
@@ -32,12 +36,12 @@ let displayHolidays = (holidays) => {
 }
 
 // if (props.currentCountry !== "") {
-if (!!holidays) {
+if (!!props.currentCountry) {
     console.log(holidays)
     return(
         <div className="country-details">
             <div className="name">{props.currentCountryName}</div>
-            <div className="holidayHeader holiday"><div className="holidayName">Holiday</div><div className="holidayDate">Date</div><div className="holidayType">Type</div></div>
+            <div className="holidayHeader holiday"><div className="holidayName">Holiday</div><div className="holidayDate"><img className="arrow" src={leftArrow} alt="Previous month"></img>{new Date().toLocaleString('default', { month: 'long' })} {new Date().getFullYear().toString()}<img className="arrow" src={rightArrow} alt="Next month"></img></div><div className="holidayType">Type</div></div>
             {displayHolidays(holidays)}
         </div>
     )
