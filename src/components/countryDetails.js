e24522d1d07ad5aa527cfdb063721f7f884f879a
Allen <Allenlea2007@gmail.com>
2021-04-25 12:39:38 -0500

    cleaned up notes inside of code

--- src/components/countryDetails.js
+++ src/components/countryDetails.js
@@ -5,18 +5,11 @@ export function CountryDetails(props) {
 	const [holidays, setHolidays] = useState([]);
 
 	let getCountryHolidays = (countryCode) => {
-		// let year = '2020';
+
 		let year = new Date().getFullYear().toString();
 
-		//Holiday API key
-		// const apiKey = '580d9214-e005-4391-9de5-8b111c8bbd0e';
-		// fetch(`https://holidayapi.com/v1/holidays/?key=${apiKey}&country=${countryCode}&year=${year}`)
-		//Calendarific API key
 		const apiKey = "a207d49e5afb124557eee979ab1b7ca2cc315135";
-		fetch(
-			`https://calendarific.com/api/v2/holidays?&api_key=${apiKey}&country=${countryCode}&year=${year}`
-		)
-			// when using the API above, replace observed with holiday type
+		fetch(`https://calendarific.com/api/v2/holidays?&api_key=${apiKey}&country=${countryCode}&year=${year}`)
 			.then((response) => response.json())
 			.then((results) => {
 				setHolidays(results.response.holidays);
@@ -28,7 +21,6 @@ export function CountryDetails(props) {
 	},[props.currentCountry]);
 
 	let displayHolidays = (holidays) => {
-		// return holidays.map( holiday => <div className="holiday"><div className="holidayName">{holiday.name}</div> <div className="holidayDate">{holiday.date}</div><div className="holidayObserved">{holiday.date !== holiday.observed ? holiday.observed : " "}</div></div> )
 		return holidays.map((holiday) => (
 			<div className="holiday">
 				<div className="holidayName" data-tip data-for={holiday.name}>
@@ -50,7 +42,6 @@ export function CountryDetails(props) {
 		));
 	};
 
-	// if (props.currentCountry !== "") {
 	if (!!holidays) {
 		return (
 			<div className="country-details">
